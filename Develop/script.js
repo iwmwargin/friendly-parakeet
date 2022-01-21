7f5dc122ec2b84ac951b3bc7b602802f30ba1fdb
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-01-21 15:37:10 -0500

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,41 @@
-// Assignment code here
+alert("Let's make a password!");
 
-//test
+//Global Variables
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "`~!@#$%^&*()_-=+/.,<>?;:'][}{";
+
+
+// Function that generates the password
+function generatePassword() {
+  // Prompt that will retrieve character length
+  var length = prompt("How many characters would you like your password to be? (Pick between 8 and 128 characters)");
+
+    while (length < 8 || length > 128) {
+      length = prompt("Your password must be between 8 and 128 characters. How many characters would you like it to be?");
+    }
+      // Continues once user input is validated
+      var confirmNumber = confirm("Will this contain numbers?");
+
+
+      var confirmCharacter = confirm("Will this contain special characters?");
+      var confirmUppercase = confirm("Will this contain Uppercase letters?");
+      var confirmLowercase = confirm("Will this contain Lowercase letters?");
+ 
+
+    var uppercase = confirm()
+
+
+//prompt user for the password criteria
+// Password length 8 to 128 characters
+//
+
+
+  return "generated password goes here"
+}
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
