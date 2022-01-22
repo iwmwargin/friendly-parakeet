c5b93e6a611135fcdb8874b092146162ee8ee16f
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-01-22 14:29:37 -0500

    changing file path

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//alert("Let's make a password!");
+alert("Let's make a password!");
 
 //List of available characters
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,6 +15,7 @@ function generatePassword() {
   // Prompt that will retrieve character length
   var length = prompt("How many characters would you like your password to be? (Pick between 8 and 128 characters)");
 
+  // While loop that will specify length of password
     while (length < 8 || length > 128) {
       length = prompt("Your password must be between 8 and 128 characters. How many characters would you like it to be?");
     }
@@ -25,6 +26,7 @@ function generatePassword() {
       var confirmUppercase = confirm("Will this contain Uppercase letters?");
       var confirmLowercase = confirm("Will this contain Lowercase letters?");
 
+// If statements that determine what character sets the user wants
       if (confirmNumber) {
         passwordChar += numbers;
       }
@@ -44,6 +46,7 @@ function generatePassword() {
         passwordChar += lowercase;
       }
 
+// Makes sure that the user has to select at least one valid character set.
       if (
         !confirmLowercase && 
         !confirmUppercase && 
@@ -53,13 +56,13 @@ function generatePassword() {
           alert("You must choose at least one valid character set. Please try again.");
           generatePassword();
         }
-
+// For loop that will take the characters selected by user and create the random password
       for (var i = 0; i < length; i++) {
         
          password += passwordChar[Math.floor(Math.random() * passwordChar.length)];
       }
 
-
+// This will send the password back to the function
   return password;
 }
 
@@ -78,3 +81,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
